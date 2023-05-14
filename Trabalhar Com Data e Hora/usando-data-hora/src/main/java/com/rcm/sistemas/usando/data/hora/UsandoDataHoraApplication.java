bdd33f0c4d81f3909de5c67c280fef3ba945ee09
= <=>
2023-05-14 15:52:12 -0300

    Commit Inicial

---
+++ Trabalhar Com Data e Hora/usando-data-hora/src/main/java/com/rcm/sistemas/usando/data/hora/UsandoDataHoraApplication.java
@@ -0,0 +1,13 @@
+package com.rcm.sistemas.usando.data.hora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsandoDataHoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsandoDataHoraApplication.class, args);
+	}
+
+}
---
+++ Trabalhar Com Data e Hora/usando-data-hora/src/test/java/com/rcm/sistemas/usando/data/hora/UsandoDataHoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcm.sistemas.usando.data.hora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsandoDataHoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
